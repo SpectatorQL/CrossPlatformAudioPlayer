a67a9cf4d16b9ef9994acb9587941f762a7c0b4e
SpectatorQL <HLcombine@gmail.com>
2018-06-09 13:18:29 +0200

    - some syntactic sugar over here

--- CPAP/CPAP/AudioPlayerUWP.cs
+++ CPAP/CPAP/AudioPlayerUWP.cs
@@ -9,9 +9,7 @@ namespace CPAP
 
         protected override void LoadAudioFile()
         {
-            if (_audioStream != null)
-                _audioStream.Dispose();
-
+            _audioStream?.Dispose();
             _audioStream = new FileStream(CurrentSong.FileHandle, FileAccess.Read);
             _player.Load(_audioStream);
         }
